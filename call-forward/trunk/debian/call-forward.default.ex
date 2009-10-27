20f66b83a3ef1f25a9528be6fae38d1af8cc74ee
cobb <cobb@f8551043-cd12-4480-adfb-fda4d363dc23>
2009-10-27 20:17:39 +0000

    call-forward: add debian directory

---
+++ call-forward/trunk/debian/call-forward.default.ex
@@ -0,0 +1,10 @@
+# Defaults for call-forward initscript
+# sourced by /etc/init.d/call-forward
+# installed at /etc/default/call-forward by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
